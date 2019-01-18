3d638cd9195fea11cac49194275b8ab54f18933e
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-18 22:59:10 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,9 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  def change
+    create_table :Landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+    end
+  end
+  # raise 'Write CreateLandmarks migration here'
 end
